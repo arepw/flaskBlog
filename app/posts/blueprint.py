e3db3e2156d31c7f32f060a2f285a6bb49f78e6d
Артем <artemii.samohin@gmail.com>
2022-09-07 23:22:56 +1000

    Added tag_details view and template. Added card template for posts.html and tag_detail.html templates to not violate the D.R.Y. principle.

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -14,3 +14,9 @@ def posts_list():
 def post_details(slug):
     post = Post.query.filter(Post.slug == slug).first()
     return render_template('posts/post_detail.html', post=post)
+
+
+@posts.route('/tags/<slug>')
+def tag_details(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    return render_template('posts/tag_detail.html', tag=tag)
