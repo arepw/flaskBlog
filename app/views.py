52c2f95dd2a50e989924bd99942614d45a69e94a
Артем <artemii.samohin@gmail.com>
2022-09-09 22:53:15 +1000

    The search from the index page now redirects to /blog/ with the search ("?q=") argument, so now it works fine. The search button on the navigation bar is no longer "outlined", so it has become brighter.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from app import app
 from models import Post, Tag
 
 
 @app.route('/')
 def index():
+    search_query = request.args.get('q')
+    if search_query:
+        return redirect(url_for('posts.posts_list', q=search_query))
     return render_template('index.html')
