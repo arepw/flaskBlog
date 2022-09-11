76a3939aa6880cfa63f1017179418b6f35a34646
Артем <artemii.samohin@gmail.com>
2022-09-11 13:01:11 +1000

    Started adding roles functionality. Now admin or user with role "editor" can press the edit button in post details to edit post outside the admin panel.

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from flask_security import login_required
+from flask_security import login_required, roles_accepted, current_user
 from models import Post, Tag
 from app import db
 from .forms import PostForm
@@ -9,6 +9,7 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/create', methods=['POST', 'GET'])
 @login_required
+@roles_accepted('admin', 'editor')
 def post_create():
     form = PostForm()
     if request.method == 'POST':
@@ -48,7 +49,10 @@ def posts_list():
 @posts.route('/<slug>')
 def post_details(slug):
     post = Post.query.filter(Post.slug == slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    # If user have at least one of the roles return True to show "Edit" button.
+    return render_template('posts/post_detail.html', post=post,
+                           edit=any(map(current_user.has_role, ('admin', 'editor')))
+                           )
 
 
 @posts.route('/tags/<slug>')
@@ -59,6 +63,7 @@ def tag_details(slug):
 
 @posts.route('/<slug>/edit', methods=['POST', 'GET'])
 @login_required
+@roles_accepted('admin', 'editor')
 def post_update(slug):  # TODO: Show edit button in the view
     post = Post.query.filter(Post.slug == slug).first()
     if request.method == 'POST':
